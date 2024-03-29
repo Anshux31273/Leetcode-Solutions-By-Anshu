a07337674744ec4d22e16176a5bfd57951dd62e0
Anshu Srivastava <100195276+Anshux31273@users.noreply.github.com>
2024-03-29 14:20:16 +0530

    Update Q.49. Group Anagrams.cpp
    Time: O(n)
    Space: O(1)

--- Q.49. Group Anagrams.cpp
+++ Q.49. Group Anagrams.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> res;
-        unordered_map<string, vector<int>> m;
-        for(int i = 0;i < strs.size();i ++)
-        {
-            string s = strs[i];
-            sort(s.begin(), s.end());
-            m[s].push_back(i);
+        unordered_map<string, vector<string>> mp;
+        
+        for(auto x: strs){
+            string word = x;
+            sort(word.begin(), word.end());
+            mp[word].push_back(x);
         }
-        for(auto i : m)
-        {
-            vector<string> temp;
-            for(auto j : i.second)
-            {
-                temp.push_back(strs[j]);
-            }
-            res.push_back(temp);
+        
+        vector<vector<string>> ans;
+        for(auto x: mp){
+            ans.push_back(x.second);
         }
-        return res;
+        return ans;
     }
 };
