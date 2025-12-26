fada663be49b50048d27a7b067a13d8452f6a56c
Anshu Srivastava <100195276+Anshux31273@users.noreply.github.com>
2025-12-26 23:32:21 +0530

    Time: 107 ms (44.46%) | Memory: 22.9 MB (25.32%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -0,0 +1,22 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        precum = [0] * len(customers)
+        n = 0
+        for i in range(len(customers)):
+            if customers[i] == 'Y':
+                if i == 0:
+                    precum[0] = 1
+                else:
+                    precum[i] = precum[i - 1] + 1
+            else:
+                precum[i] = precum[i - 1] - 1
+        max, ans = 0, 0
+        print(precum)
+
+        for i in range(len(precum)):
+            if precum[i] > max:
+                max = precum[i]
+                ans = i
+        if max <= 0:
+            return 0
+        return ans + 1
